c10d535d8fc639afa06eed7e6ff8ea73b6740950
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2021-02-12 13:09:08 -0300

    Iniciando repositório

---
+++ README.md
@@ -0,0 +1,19 @@
+# Projeto Tche Games Switch- Versão React JS e Node JS
+
+Mais um repositório do Projeto Tche Games Switch
+
+Esse projeto é parte do curso de desenvolvedor Full Stack da Recode Pro 2020.
+
+Os objetivos nesse projeto foram:
+* Configurar um servidor HTTP com Node.Js
+* Realizar a conexão com o banco de dados
+* Criar uma interface para retorno do dados de BD em formato JSON
+* Realizar o refactory (reconstrução) do projeto substituindo a parte back-end anteriormente construída em PHP pelo back-end Node.Js.
+
+
+## Instruções de instalação e execução:
+* Realizar o clone deste projeto;
+* Instalar as dependências na pasta raíz com o comando: npm install;
+* Instalar o banco de dados (script na pasta /src/bancodados/tchegames0102;
+* Executar o servidor Node JS contido na pasta src/ com o commando nodemon servidor.js;
+* Executar o projeto React JS na pasta /src com o comando npm start;
---
+++ src/servidor.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const conexao = require('./bancodados/conexao');
+
+app.use(cors({origin: 'http://localhost:3000'}));
+
+app.use(bodyParser.urlencoded({ extend: true}));
+app.use(express.json());
+
+
+app.get('/selecttodos', (req, res) => {
+    conexao.query("SELECT * FROM produtos;", (error, result) => {
+        res.json(result);  
+    })
+});
+
+
+app.get('/selectconsoles', (req, res) => {
+    conexao.query("SELECT * FROM produtos where categoria='consoles';", (error, result) => {
+        res.json(result);  
+    })
+});
+
+app.get('/selectjogos', (req, res) => {
+    conexao.query("SELECT * FROM produtos where categoria='jogos';", (error, result) => {
+        res.json(result);  
+    })
+});
+
+app.get('/selectacessorios', (req, res) => {
+    conexao.query("SELECT * FROM produtos where categoria='acessorios';", (error, result) => {
+        res.json(result);  
+    })
+});
+
+
+
+
+app.post("/insertpedido",(req,res) => {
+    let pedido = []
+    
+    pedido.push({
+        nomeClientes: req.body.nomeClientes,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        endereco: req.body.endereco,
+        produto_id: req.body.produto_id,
+        quantidade: req.body.quantidade
+    })
+    conexao.query('INSERT INTO pedidos SET ?', pedido, () =>{ 
+      
+    })
+  });
+
+  app.get("/selectcontrole", (req,res) => {
+    conexao.query("SELECT * FROM pedidos INNER JOIN produtos on pedidos.produto_id = produtos.idprodutos;",(error,result) => {
+      res.json(result)
+    })
+  });
+
+
+
+
+
+
+
+app.listen(3001);
